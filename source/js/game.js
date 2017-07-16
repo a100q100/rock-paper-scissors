cb5f2fa2570b84c1beacad080811ce17eeaa92ea
Renato Pereira <renato.ppontes@gmail.com>
2017-07-16 18:09:32 -0300

    Initiall tests for Game: properties and #setGameMode

--- source/js/game.js
+++ source/js/game.js
@@ -1,3 +1,134 @@
 export default class Game {
-  constructor() {}
+  constructor() {
+    this._score1 = 0
+    this._score2 = 0
+
+    this._gameModeId = null
+    this._gameModeName = null
+    this._gameModeShapes = null
+    this._gameModeRules = null
+  }
+
+  get score1() {
+    return this._score1
+  }
+
+  get score2() {
+    return this._score2
+  }
+
+  setGameMode(mode) {
+    // Validate mode type
+    if (!mode) {
+      throw new Error(`Invalid mode type.`)
+    }
+
+    // Validate mode id
+    if (typeof mode.id !== 'string') {
+      throw new Error(`Mode id must be a string. `+
+                      `You provided "${mode.id}" instead.`)
+    }
+
+    // Validate mode name
+    if (typeof mode.name !== 'string') {
+      throw new Error(`Mode name must be a string. `+
+                      `You provided "${mode.name}" instead.`)
+    }
+
+    // Validate mode shapes
+    this._validateShapes(mode)
+
+    // Validate mode rules
+    this._validateRules(mode)
+
+    this._gameModeId = mode.id
+    this._gameModeName = mode.name
+    this._gameModeShapes = mode.shapes
+    this._gameModeRules = mode.rules
+  }
+
+  _validateShapes(mode) {
+    // Validate shape type
+    if (!Array.isArray(mode.shapes)) {
+      throw new Error(`Invalid shape type.`)
+    }
+
+    // Validate shape length
+    if (mode.shapes.length <= 2) {
+      throw new Error(`Mode must have at least 3 shapes.`)
+    }
+
+    // Validate shape elements
+    let shapesIds = []
+    let shapesLabels = []
+    for (let i=0; i<mode.shapes.length; i++) {
+      let shape = mode.shapes[i]
+
+      // Validate element type and required fields
+      if (!shape || !shape.id || !shape.label) {
+        throw new Error(`Invalid shape element.`)
+      }
+
+      // Validate duplicated shape id 
+      if (shapesIds.indexOf(shape.id) >= 0) {
+        throw new Error(`Duplicated shape id "${shape.id}".`)
+      }
+
+      // Validate duplicated shape label
+      if (shapesLabels.indexOf(shape.label) >= 0) {
+        throw new Error(`Duplicated shape label "${shape.label}".`)
+      }
+
+      shapesIds.push(shape.id)
+      shapesLabels.push(shape.label)
+    }
+  }
+
+  _validateRules(mode) {
+    // Validate rules type
+    if (!mode.rules || !Object.keys(mode.rules)) {
+      throw new Error(`Invalid rules type.`)
+    }
+
+    const shapes = mode.shapes.map(x => x.id)
+    let keyPool = shapes.map(x => x)
+    let valuePool = shapes.map(x => x)
+
+    // Validate rules elements
+    // eg: rules = {'a': [...], 'b': [...]}
+    let keys = Object.keys(mode.rules)
+    for (let i=0; i<keys.length; i++) {
+      // eg: key = 'a'
+      // eg: values = ['b', 'c']
+      let key = keys[i]
+      let values = mode.rules[key]
+
+      // Validate rule id
+      if (shapes.indexOf(key) === -1) {
+        throw new Error(`Invalid rules element.`)
+      }
+      let keyIndex = keyPool.indexOf(key)
+      if (keyIndex >= 0) {
+        keyPool.splice(keyIndex, 1)
+      }
+
+      // Validate rule values
+      for (let j=0; j<values.length; j++) {
+        let value = values[j]
+
+        if (shapes.indexOf(value) === -1) {
+          throw new Error(`Invalid rules element.`)
+        }
+        let valueIndex = valuePool.indexOf(value)
+        if (valueIndex >= 0) {
+          valuePool.splice(valueIndex, 1)
+        }
+      }
+    }
+
+    // Validate unused shapes
+    if (keyPool.length || valuePool.length) {
+      throw new Error(`Missing shape.`)
+    }
+  }
 }
--- test/source/game.test.js
+++ test/source/game.test.js
@@ -1,6 +1,136 @@
+const MODES = require('../../source/js/gameModes.js').default
+
+function _createMode() {
+  return JSON.parse(JSON.stringify(MODES.classical))
+}
+
 describe('Game', () => {
-  it('should have score property (for both players)')
-  it('should load a game mode (from rule specification)')
+  let Game = null
+
+  before(() => {
+    Game = require('../../source/js/game.js').default
+  })
+
+  it('should have score property (for both players)', () => {
+    let game = new Game()
+
+    // Testing existance and initial value
+    assert.equal(game.score1, 0)
+    assert.equal(game.score2, 0)
+
+    // Testing read only
+    assert.throws(() => {
+      game.score1 = 10
+    })
+    assert.equal(game.score1, 0)
+
+    // Testing non-initial value
+    game._score1 = 5
+    assert.equal(game.score1, 5)
+    assert.equal(game.score2, 0)
+  })
+
+  describe('#setGameMode', () => {
+    it('should load a game mode (from rule specification)', () => {
+      let game = new Game()
+      let mode = MODES.classical
+
+      game.setGameMode(mode)
+      assert.equal(game._gameModeId, mode.id)
+      assert.equal(game._gameModeName, mode.name)
+      assert.deepEqual(game._gameModeShapes, mode.shapes)
+      assert.deepEqual(game._gameModeRules, mode.rules)
+    })
+
+    it('should validate invalid mode type', () => {
+      let game = new Game()
+
+      assert.throws(() => {
+        game.setGameMode(null)
+      }, 'Invalid mode type')
+    })
+
+    it('should validate mode ID', () => {
+      let game = new Game()
+      let mode = _createMode()
+      mode.id = 1
+
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Mode id must be a string')
+    })
+
+    it('should validate mode name', () => {
+      let game = new Game()
+      let mode = _createMode()
+      mode.name = 1
+
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Mode name must be a string')
+    })
+
+    it('should validate mode shapes', () => {
+      let game = new Game()
+      let mode = _createMode()
+
+      mode.shapes = {}
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Invalid shape type', null, 'Did not throw on invalid shape type.')
+
+      mode.shapes = [{id:'a', label:'A'}, {id:'b', label:'B'}]
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Mode must have at least 3 shapes', null, 'Did not throw on invalid shape length.')
+
+      mode.shapes = [{id:'a', label:'A'}, {invaliid:'b', label:'B'}, {id:'c', label:'C'}]
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Invalid shape element', null, 'Did not throw on invalid shape element.')
+
+      mode.shapes = [{id:'a', label:'A'}, {id:'a', label:'B'}, {id:'c', label:'C'}]
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Duplicated shape id', null, 'Did not throw on duplicated shape id.')
+
+      mode.shapes = [{id:'a', label:'A'}, {id:'b', label:'B'}, {id:'c', label:'B'}]
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Duplicated shape label', null, 'Did not throw on duplicated shape label.')
+    })
+
+    it('should validate mode rules', () => {
+      let game = new Game()
+      let mode = _createMode()
+
+      mode.rules = null
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Invalid rules type', null, 'Did not throw on invalid rules type.')
+
+      mode.rules = {'invalid': ['']}
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Invalid rules element', null, 'Did not throw on invalid rules element.')
+
+      mode.rules = {'paper': ['invalid'], 'rock':['scissor'], 'scissor':['paper']}
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Invalid rules element', null, 'Did not throw on invalid rules element.')
+
+      mode.rules = {'paper': ['scissor'], 'rock':['scissor'], 'scissor':['paper']}
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Missing shape', null, 'Did not throw on missing shape.')
+
+      mode.rules = {'rock':['scissor'], 'scissor':['paper']}
+      assert.throws(() => {
+        game.setGameMode(mode)
+      }, 'Missing shape', null, 'Did not throw on missing shape.')
+    })
+  })
+
   it('should generate a random play')
   it('should return list of all possible plays')
   it('should check which player won the round')

de365465456c7b4e030e4772b75e28d4ba3e6123
Renato Pereira <renato.ppontes@gmail.com>
2017-07-16 19:16:24 -0300

    Game small refactor and documentation

--- source/js/game.js
+++ source/js/game.js
@@ -1,4 +1,11 @@
+/**
+ * The Game class is the model of our MVC achitecture, it handles all the game
+ * rules and score management.
+ */
 export default class Game {
+  /**
+   * Constructor.
+   */
   constructor() {
     this._score1 = 0
     this._score2 = 0
@@ -9,14 +16,57 @@ export default class Game {
     this._gameModeRules = null
   }
 
+  /**
+   * The player 1 score. Default to 0 (zero).
+   * @type {Number}
+   */
   get score1() {
     return this._score1
   }
 
+  /**
+   * The player 2 score. Default to 0 (zero).
+   * @type {Number}
+   */
   get score2() {
     return this._score2
   }
 
+  /**
+   * By receiving a game specification, this method validates and setup the 
+   * game mode. 
+   *
+   * The specification must be an object that respects the following structure:
+   *
+   * - **id**: a string with the ID of the game mode.
+   * - **name**: a string with the name of the game mode.
+   * - **shapes**: an array of objects in the format:
+   *   - **id**: a string with the ID of the shape, must be unique.
+   *   - **label**: a string with the label of the shape, must be unique.
+   * - **rules**: an object where keys are shape IDs and values are arrays of
+   *   shape IDs. This dict represents the shapes (keys) that won over other
+   *   shapes (values).
+   *
+   * For instance, the classical rock-paper-scissors game can be represented
+   * as follows:
+   *
+   *     {
+   *       id: 'classical',
+   *       name: 'Classical',
+   *       shapes: [
+   *         {id:'paper', label:'Paper'},
+   *         {id:'rock', label:'Rock'},
+   *         {id:'scissor', label:'Scissor'}
+   *       ],
+   *       rules: {
+   *         'paper': ['rock'],
+   *         'scissor': ['paper'],
+   *         'rock': ['scissor']
+   *       }
+   *     }
+   *
+   * @param {Object} mode - The game mode specification (as described above).
+   */
   setGameMode(mode) {
     // Validate mode type
     if (!mode) {
@@ -47,6 +97,88 @@ export default class Game {
     this._gameModeRules = mode.rules
   }
 
+  /**
+   * Returns a random possible shape id.
+   *
+   * @returns {String} The shape id (eg. 'paper' or 'scissor')
+   */
+  getRandomShapeId() {
+    let i = Math.floor(Math.random()*this._gameModeShapes.length)
+    return this._gameModeShapes[i].id
+  }
+
+  /**
+   * Returns the list of all shapes, with id and label. For example:
+   *
+   *     [
+   *       {id:'paper', label:'Paper'},
+   *       {id:'rock', label:'Rock'},
+   *       {id:'scissor', label:'Scissor'}
+   *     ]
+   *
+   * @returns {Object} The shapes object.
+   */
+  getAllShapes() {
+    // We copy the shape so it can't be mutated
+    return JSON.parse(JSON.stringify(this._gameModeShapes))
+  }
+
+  /**
+   * Checks which player won for a given set of shapes. It returns `0` if the
+   * player 1 won, `1` if the player 2 won, and `-1` if it is tied.
+   *
+   * @param {String} shape1 - The player 1 chosed shape.
+   * @param {String} shape2 - The player 2 chosed shape.
+   * @returns {Number}
+   */
+  check(shape1, shape2) {
+    let cond1 = this._gameModeRules[shape1]
+    let cond2 = this._gameModeRules[shape2]
+
+    if (cond1 && cond1.indexOf(shape2) >= 0) {
+      return 0
+    } else if (cond2 && cond2.indexOf(shape1) >= 0) {
+      return 1
+    } else {
+      return -1
+    }
+  }
+
+  /**
+   * Resets the game by setting the score to 0 for both players.
+   */
+  reset() {
+    this._score1 = 0
+    this._score2 = 0
+  }
+
+  /**
+   * Checks which player won for a given set of shapes and updates the score of
+   * the winner. It returns the same values of {@check} function.
+   *
+   * @param {String} shape1 - The player 1 chosed shape.
+   * @param {String} shape2 - The player 2 chosed shape.
+   * @returns {Number}
+   */
+  play(shape1, shape2) {
+    let won = this.check(shape1, shape2)
+
+    if (won === 0) {
+      this._score1 += 1
+    }
+    else if (won === 1) {
+      this._score2 += 1
+    }
+
+    return won
+  }
+
+  /**
+   * Internal method to help the `mode.shapes` validation. It receives the mode
+   * specification and throws an error if the specification is invalid.
+   *
+   * @param {Object} mode - The game mode specification.
+   */
   _validateShapes(mode) {
     // Validate shape type
     if (!Array.isArray(mode.shapes)) {
@@ -84,6 +216,12 @@ export default class Game {
     }
   }
 
+  /**
+   * Internal method to help the `mode.rules` validation. It receives the mode
+   * specification and throws an error if the specification is invalid.
+   * 
+   * @param {Object} mode - The game mode specification.
+   */
   _validateRules(mode) {
     // Validate rules type
     if (!mode.rules || !Object.keys(mode.rules)) {
@@ -130,45 +268,5 @@ export default class Game {
     if (keyPool.length || valuePool.length) {
       throw new Error(`Missing shape.`)
     }
-  }
-
-  getRandomShapeId() {
-    let i = Math.floor(Math.random()*this._gameModeShapes.length)
-    return this._gameModeShapes[i].id
-  }
-
-  getAllShapes() {
-    return JSON.parse(JSON.stringify(this._gameModeShapes)) // copy
-  }
-
-  check(shape1, shape2) {
-    let cond1 = this._gameModeRules[shape1]
-    let cond2 = this._gameModeRules[shape2]
-
-    if (cond1 && cond1.indexOf(shape2) >= 0) {
-      return 0
-    } else if (cond2 && cond2.indexOf(shape1) >= 0) {
-      return 1
-    } else {
-      return -1
-    }
-  }
-
-  reset() {
-    this._score1 = 0
-    this._score2 = 0
-  }
-
-  play(shape1, shape2) {
-    let won = this.check(shape1, shape2)
-
-    if (won === 0) {
-      this._score1 += 1
-    }
-    else if (won === 1) {
-      this._score2 += 1
-    }
-
-    return won
   }
 }
